a3bbd591a4dced887b771d107683390098f72952
fujitashuichi <fujitashuu14@gmail.com>
2026-08-21 21:56:00 +0900

    feature: バインディングの練習

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,28 +1,44 @@
-﻿namespace MauiApp1;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
-public partial class MainPage : ContentPage
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
-	public string? UserName { get; private set; } = null;
+	private string _text = string.Empty;
+
+	public string Text
+	{
+		get => _text;
+		set
+		{
+			if (_text != value) {
+				_text = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public ICommand ClearTextCommand { get; }
+
 
 	public MainPage()
 	{
 		InitializeComponent();
+
+		ClearTextCommand = new Command(() =>
+		{
+			Text = string.Empty;
+		});
+		BindingContext = this;
 	}
 
-	private void OnTextChanged(object? sender, TextChangedEventArgs e)
-	{
-		UserName = e.NewTextValue;
-	}
 
-	private void OnUserNameApply(object? sender, EventArgs e)
+	public new event PropertyChangedEventHandler? PropertyChanged;
+
+	protected override void OnPropertyChanged([CallerMemberName] string? propertyName = null)
 	{
-		if (string.IsNullOrWhiteSpace(UserName))
-		{
-			MainText.Text = "Who are you?";
-		}
-		else
-		{
-			MainText.Text = $"Hello {UserName}!";
-		}
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 }
